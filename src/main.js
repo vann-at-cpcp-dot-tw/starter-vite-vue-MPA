80d48c2b791aa999ba7af88f07253b27e69d1dff
Vann Liu <vann@cpcptw>
2022-02-26 23:07:09 +0800

    temp

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+// 新增物件原型 replaceAll 方法 (支援舊手機)
+if (!String.prototype.replaceAll) {
+  String.prototype.replaceAll = function(str, newStr){
+
+      function escapeRegExp(string) {
+          return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
+      }
+
+      return this.replace(new RegExp(escapeRegExp(str), 'g'), newStr)
+  }
+}
+
 // https://vitejs.dev/config/#build-polyfillmodulepreload
 // using workaround https://github.com/vitejs/vite/issues/4786
 if (import.meta.env.MODE !== 'development') {
